cbf9fc341ade5cdf2d3a7b9bc26226045886149c
Salvador Huertas <shuertas@telefonica.net>
2023-12-26 23:43:00 +0100

    Initial Commit

---
+++ RutinasTreu/RutinasTreu.h
@@ -0,0 +1,18 @@
+//
+//  RutinasTreu.h
+//  RutinasTreu
+//
+//  Created by Salvador Huertas on 26/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RutinasTreu.
+FOUNDATION_EXPORT double RutinasTreuVersionNumber;
+
+//! Project version string for RutinasTreu.
+FOUNDATION_EXPORT const unsigned char RutinasTreuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RutinasTreu/PublicHeader.h>
+
+
